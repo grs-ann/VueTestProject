334e98025146e14b4cd845d559e457d3741ecfaf
grs-ann <pv.forwork@gmail.com>
2021-03-24 15:07:37 +0400

    UserProfile component has beed added; Minor changes in App.vue

---
+++ vuejs/src/components/UserProfile.vue
@@ -0,0 +1,93 @@
+<template>
+  <div class="user-profile">
+      <div class="user-profile__user-panel">
+          <h1 class="user-profile__username">@{{ user.username }}</h1>
+          <div class="user-profile__admin-badge" v-if="user.isAdmin">Admin</div>
+          <div class="user-profile__admin-badge" v-else>Default user</div>
+          <div class="user-profile__follower-count">
+              <strong>Followers: </strong> {{ followers }}
+          </div>
+      </div>
+      <div class="user-profile__twoots-wrapper">
+              <div class="user-profile__twoot" v-for="twoot in user.twoots" :key="twoot.id" >
+                  {{ twoot.content }}
+              </div>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "UserProfile",
+    data() {
+        return {
+            followers: 0,
+            user: {
+                id: 1,
+                username: "42ama",
+                firstname: "Maxim",
+                lastname: "Alonov",
+                email: "42ama@gmail.com",
+                isAdmin: true,
+                twoots: [
+                    { id: 1, content: 'Twotter is Amazing!'},
+                    { id: 2, content: "Don't forget to subscribe!" },
+                    { id: 3, content: "grs's legs is a very beautifull!"}
+                ]
+            }
+        }
+    },
+    watch: {
+        followers(newFollowerCount, oldFollowerCount) {
+            if (oldFollowerCount < newFollowerCount) {
+                console.log(`${this.user.username} has gained a follower!`);
+            }
+        }
+    },
+    methods: {
+        followUser() {
+            this.followers++;
+        }
+    },
+    computed: {
+        fullName() {
+            return `${this.user.firstname} ${this.user.lastname}`;
+        }
+    },
+    mounted() {
+        this.followUser();
+    }
+}
+</script>
+
+<style>
+.user-profile {
+    display: grid;
+    grid-template-columns: 1fr 3fr;
+    width: 100%;
+    padding: 50px 5%;
+}
+
+.user-profile__user-panel {
+    display: flex;
+    flex-direction: column;
+    margin-right: 50px;
+    padding: 20px;
+    background-color: white;
+    border-radius: 5px;
+    border: 1px solid #dfe3e8;
+}
+.user-profile__admin-badge {
+    background: yellowgreen;
+    color: white;
+    border-radius: 5px;
+    margin-right: auto;
+    padding: 0 10px;
+    font-weight: bold;
+}
+
+h1 {
+    margin: 0;
+}
+
+</style>
